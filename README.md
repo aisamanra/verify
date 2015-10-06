97424494cf2b4ec40b769ac0ac0907decc719dd6
Getty Ritter <gettylefou@gmail.com>
2015-10-05 20:41:07 -0700

    First pass at verify utility

---
+++ README.md
@@ -0,0 +1,3 @@
+# Verify
+
+[ describe more here ]
---
+++ verify.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -e
+
+TRUSTDIR=${TRUSTDIR-"$HOME/.trusted"}
+
+if [ ! "$(which signify)" ]; then
+	echo "Unable to find signify utility" >&2
+	exit 111
+fi
+
+if [ ! "$(ls -A $TRUSTDIR)" ]; then
+	echo "No trusted keys in $TRUSTDIR" >&2
+	exit 111
+fi
+
+TGT=`mktemp -d`
+cd $TGT
+tar -xf -
+if [ ! -e ./sig ]; then
+	echo "Malformed data: missing signature" >&2
+	cd .. && rm -rf $TGT
+	exit 111
+elif [ ! -e ./dat ]; then
+	echo "Malformed data: missing payload" >&2
+cd .. && rm -rf $TGT
+	exit 111
+else
+	for pub in $TRUSTDIR/*; do
+		if signify -Vq -p $pub -m ./dat -x ./sig; then
+			cat ./dat
+			cd / && rm -rf $TGT
+			exit 0
+		fi
+	done
+	echo "Unable to verify file" >&2
+	cd .. && rm -rf $TGT
+	exit 111
+fi
